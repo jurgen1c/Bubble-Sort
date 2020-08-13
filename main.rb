8577b1dadb7660d5c93be1422950154e879b1771
hguruman <hguruman@gmail.com>
2020-08-14 02:07:47 +0400

    Added bubble_sort method

--- main.rb
+++ main.rb
@@ -1,1 +1,26 @@
-puts "Hello Bubble Sort"
+def bubble_sort(arr)
+    arr_length = arr.length
+    return arr if arr_length <= 1
+
+    loop do 
+
+        swapped = false
+
+        (arr_length-1).times do |counter|
+            if arr[counter] > arr[counter + 1]
+                arr[counter], arr[counter + 1] = arr[counter + 1], arr[counter]
+                swapped = true
+
+            end
+        end
+
+        break if not swapped
+    end
+
+    arr
+end
+
+test_array = [25, 54, 8, 4, 46, 1, 7]
+
+puts bubble_sort(test_array)
+
