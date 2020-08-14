7170d7f73ee5cc13cfb975e86e64ce3d07d58597
hguruman <hguruman@gmail.com>
2020-08-14 19:08:21 +0400

    Added bubble_sort_by method

--- main.rb
+++ main.rb
@@ -26,6 +26,29 @@ my_array = [23, 43, 1, 9, 82, 51];
 
 p bubble_sort(my_array);
 
+
+def bubble_sort_by(array)
+    if array.all? String
+      k = array.length - 1
+      (0..k).each do |_i|
+        (0..k).each do |i|
+          next unless i != k
+          array[i], array[i + 1] = array[i + 1], array[i] if yield(array[i], array[i + 1]).positive?
+        end
+      end
+      array
+    else
+      puts 'not a valid array'
+    end
+  end
+  
+  puts 'Result of bubble_sort():'
+  print bubble_sort([4, -3, 37.3, -78, 2.4, 0, 4])
+  puts "\nResult of bubble_sort_by():"
+  print(bubble_sort_by(['hi', 'hello', 'goodbye', 'hey!']) do |left, right|
+    left.length - right.length
+  end)
+
 =begin
 def bubble_sort_by(arr)
   i = 0
