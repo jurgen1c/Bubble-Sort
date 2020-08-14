a46ba35db2941725ff31790fc4d7b3fb34daba76
hguruman <hguruman@gmail.com>
2020-08-15 00:06:33 +0400

    Fixed rubocol errors detected

--- main.rb
+++ main.rb
@@ -40,5 +40,5 @@ end
 
 puts "\nResult of bubble_sort_by():"
 print(bubble_sort_by(['hi', 'hello', 'hey!']) do |left, right|
-    left.length - right.length
-  end)
+  left.length - right.length
+end)
