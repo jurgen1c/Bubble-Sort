6914e3d45af1728e0fdb7dacba9e343518ec00d1
hguruman <hguruman@gmail.com>
2020-08-14 22:45:43 +0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,4 +1,46 @@
-puts "Hello Bubble Sort"
+def bubble_sort(arr)
+  swapped = false
+  runs = 1
 
-puts "What's up"
-name = gets.chomp
+  until swapped
+    swapped = true
+    (0...arr.length - runs).each do |i|
+      next unless arr[i] > arr[i + 1]
+
+      swapped = false
+      temp = arr[i]
+      arr[i] = arr[i + 1]
+      arr[i + 1] = temp
+
+    end
+
+    runs += 1
+
+  end
+
+  arr
+
+end
+
+my_array = [23, 43, 1, 9, 82, 51];
+
+p bubble_sort(my_array);
+
+
+def bubble_sort_by(array)
+  unsorted = true
+  while unsorted
+    (0..(array.length - 2)).each do |index|
+      if yield(array[index], array[index + 1]).negative?
+        array[index], array[index + 1] = [array[index + 1], array[index]]
+      end
+    end
+     unsorted = false
+  end
+  array
+end
+
+puts "\nResult of bubble_sort_by():"
+  print(bubble_sort_by(['hi', 'hello', 'goodbye', 'hey!']) do |left, right|
+    left.length - right.length
+  end)
