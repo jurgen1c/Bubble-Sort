0b47211b354a2095cecb40bfd854e3554acff0fe
jurgen1c <jurgen1c@gmail.com>
2020-08-14 08:56:48 -0600

    Add bubble sort method

--- main.rb
+++ main.rb
@@ -1,4 +1,55 @@
 def bubble_sort(arr)
+  swapped = false
+  runs = 1
+
+  until swapped
+    swapped = true
+    (0...arr.length - runs).each do |i|
+      next unless arr[i] > arr[i + 1]
+
+      swapped = false
+      temp = arr[i]
+      arr[i] = arr[i + 1]
+      arr[i + 1] = temp
+
+    end
+
+    runs += 1
+
+  end
+
+  arr
+
+end
+
+my_array = [23, 43, 1, 9, 82, 51];
+
+p bubble_sort(my_array);
+
+=begin
+def bubble_sort_by(arr)
+  i = 0
+  arr_l = arr.length - 1
+  while i <= arr_l
+    if yield(arr[i], arr[i + 1]).negative?
+      i += 1
+    elsif yield(arr[i], arr[i + 1]).zero?
+      i += 1
+    elsif yield(arr[i], arr[i + 1]).positive?
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      i = 0
+    end
+
+  end
+  arr
+end
+
+p bubble_sort_by([hi, hello, hey]) do |left, right|
+  left.length - right.length
+end
+
+
+def bubble_sort(arr)
     arr_length = arr.length
     return arr if arr_length <= 1
 
@@ -23,4 +74,5 @@ end
 test_array = [25, 54, 8, 4, 46, 1, 7]
 
 puts bubble_sort(test_array)
+=end
 
